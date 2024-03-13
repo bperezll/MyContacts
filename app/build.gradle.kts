ad13ec2d9b542271fdb3df6ea6a1ea668f58ba34
bperezll <borja.perez.llano@gmail.com>
2024-03-13 11:05:43 +0100

    Initial MainActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     val sqliteVersion = "2.4.0"
     implementation("androidx.sqlite:sqlite-ktx:$sqliteVersion")
 
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
