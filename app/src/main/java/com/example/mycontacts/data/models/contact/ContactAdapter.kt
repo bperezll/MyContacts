be02cf85fb4d588d15f1d9efff9bc70bdf08845f
bperezll <borja.perez.llano@gmail.com>
2024-03-13 13:46:45 +0100

    Minor changes

--- app/src/main/java/com/example/mycontacts/data/models/contact/ContactAdapter.kt
+++ app/src/main/java/com/example/mycontacts/data/models/contact/ContactAdapter.kt
@@ -4,15 +4,17 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mycontacts.databinding.ItemContactBinding
+import com.example.mycontacts.databinding.ActivityMainBinding
 
 class ContactAdapter(
     private var items:MutableList<Contact> = mutableListOf(),
     private val onClickListener: (position:Int) -> Unit,
-    private val onRemoveListener: (position:Int) -> Unit
+    private val onAddContactListener: (position:Int) -> Unit
 ) : RecyclerView.Adapter<ContactAdapter.ContactViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
         val binding = ItemContactBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        //val mainBinding = ActivityMainBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ContactViewHolder(binding)
     }
 
@@ -21,8 +23,8 @@ class ContactAdapter(
     override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
         holder.render(items[position])
         holder.itemView.setOnClickListener { onClickListener(position) }
-        /*holder.binding.deleteButton.setOnClickListener {
-            onRemoveListener(position)
+        /*holder.binding.setOnClickListener {
+            onAddContactListener(position)
         }*/
     }
 
