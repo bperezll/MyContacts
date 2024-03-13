0f0749cf01dd1a2275d3c93d42d802e0b9e88236
bperezll <borja.perez.llano@gmail.com>
2024-03-13 11:06:49 +0100

    Category object done

---
+++ app/src/main/java/com/example/mycontacts/data/models/category/Category.kt
@@ -0,0 +1,26 @@
+package com.example.mycontacts.data.models.category
+
+import com.example.mycontacts.utils.DatabaseManager
+
+class Category (private var id: Int, private var category: String) {
+
+    companion object {
+        const val TABLE_NAME = "Categories"
+        const val COLUMN_NAME_CATEGORY = "category"
+        val COLUMN_NAMES = arrayOf(
+            DatabaseManager.COLUMN_NAME_ID,
+            COLUMN_NAME_CATEGORY
+        )
+    }
+
+    /*override fun toString(): String {
+        return "$id -> Task: $name - $color"
+    }*/
+
+    /*override fun equals(other: Any?): Boolean{
+        if(other is Category){
+            return id == other.id
+        }
+        return false;
+    }*/
+}
