7c15ce53410046203457458919f2482dfeb56cc2
bperezll <borja.perez.llano@gmail.com>
2024-03-15 11:12:07 +0100

    Added Settings Activity

--- app/src/main/java/com/example/mycontacts/activities/SettingsActivity.kt
+++ app/src/main/java/com/example/mycontacts/activities/SettingsActivity.kt
@@ -2,11 +2,17 @@ package com.example.mycontacts.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.mycontacts.R
+import com.example.mycontacts.databinding.ActivitySettingsBinding
 
 class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySettingsBinding // View Binding declaration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+
+        // View binding initialization
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
