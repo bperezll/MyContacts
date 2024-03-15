c57c0c136000cc74c3bebc4165a1df4ff285f66d
bperezll <borja.perez.llano@gmail.com>
2024-03-15 12:31:14 +0100

    Change to night mode added, pending to add as a Shared Preference

--- app/src/main/java/com/example/mycontacts/activities/SettingsActivity.kt
+++ app/src/main/java/com/example/mycontacts/activities/SettingsActivity.kt
@@ -1,8 +1,10 @@
 package com.example.mycontacts.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.mycontacts.databinding.ActivitySettingsBinding
+
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -14,5 +16,14 @@ class SettingsActivity : AppCompatActivity() {
         binding = ActivitySettingsBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        // Switch to Mode Night when switch theme is checked
+        binding.switchTheme.setOnCheckedChangeListener { _, _ ->
+            if (binding.switchTheme.isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
     }
 }
