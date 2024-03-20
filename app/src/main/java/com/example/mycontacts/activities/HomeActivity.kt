d7f42e6742d089066d4c9f3c1befcd3531493730
bperezll <borja.perez.llano@gmail.com>
2024-03-20 10:43:46 +0100

    Home done

--- app/src/main/java/com/example/mycontacts/activities/HomeActivity.kt
+++ app/src/main/java/com/example/mycontacts/activities/HomeActivity.kt
@@ -1,7 +1,6 @@
 package com.example.mycontacts.activities
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Patterns
 import android.view.Menu
@@ -9,16 +8,16 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AlertDialog
-import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mycontacts.R
-import com.example.mycontacts.models.contact.Contact
-import com.example.mycontacts.models.contact.ContactAdapter
-import com.example.mycontacts.models.contact.ContactDAO
 import com.example.mycontacts.databinding.ActivityHomeBinding
 import com.example.mycontacts.databinding.DialogAddContactBinding
 import com.example.mycontacts.databinding.DialogDeleteContactBinding
+import com.example.mycontacts.models.contact.Contact
+import com.example.mycontacts.models.contact.ContactAdapter
+import com.example.mycontacts.models.contact.ContactDAO
 import com.example.mycontacts.shared.SharedFunctions
 import com.example.mycontacts.utils.SessionManager
 
